dc789189870908d19e9a481ccfb109f22f275838
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-22 01:10:08 -0300

    fix: corrige decremento do contador

--- script.js
+++ script.js
@@ -17,9 +17,15 @@ function somar() {
     document.getElementById("resultado").textContent = numero1 + numero2;
 }
 
+
 function subtrair() {
     const numero1 = Number(document.getElementById("numero1").value);
     const numero2 = Number(document.getElementById("numero2").value);
 
     document.getElementById("resultado").textContent = numero1 - numero2;
+}
+
+function diminuir() {
+    contador--;
+    document.getElementById("contador").textContent = contador;
 }
